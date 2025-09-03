ec03b1f6ce8d589bc127a86fc3e88dcb817f9d64
lhnguyenVP21 <lhnguyen21@vp.fitus.edu.vn>
2025-09-03 14:16:36 +0700

    setup axios and connect movies API

---
+++ frontend/movies-reviews/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import api from './api/axiosConfig';
+import { useState,useEffect } from 'react';
+function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/api/v1/movies');
+
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+
+
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
